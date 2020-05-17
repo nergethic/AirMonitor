d3f4d720328439d0af94b09da2a0bb42bbc05d60
Kamil Jamróz <nergydev@gmail.com>
2020-05-17 16:58:18 +0200

    new project, transition

---
+++ AirMonitor/AirMonitor/AirMonitor/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using AirMonitor.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AirMonitor
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new NavigationPage(new HomePage());
+            MainPage = new TabbedPage1();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
